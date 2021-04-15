11256d72af9a39e0a0ead939ef174df15e8fb961
Esther Chee <pnchee@gmail.com>
2021-04-15 14:28:08 +0200

    complete Post

--- src/main/java/se/kth/sda/skeleton/posts/Post.java
+++ src/main/java/se/kth/sda/skeleton/posts/Post.java
@@ -3,35 +3,53 @@ package se.kth.sda.skeleton.posts;
 import javax.persistence.*;
 import java.util.List;
 
-
-@Entity
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String title;
     private String body;
+    private String authorName;
 
     public Post() {
     }
 
-    public Post(String body) {
+    public Post(String title, String body, String authorName) {
+        this.title = title;
         this.body = body;
-    }
-
-    public Long getId() {
-        return id;
+        this.authorName = authorName;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public String getBody() {
-        return body;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getAuthorName() {
+        return authorName;
     }
 
 }
--- src/main/java/se/kth/sda/skeleton/posts/PostRepository.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostRepository.java
@@ -3,9 +3,6 @@ package se.kth.sda.skeleton.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/*
-    @TODO extend the appropriate JpaRepository to get common database operations for Post
- */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
