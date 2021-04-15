0975c917fa98a8630d8a0e7b2606ca074c7100f5
En-Chi Liu <enchi.liu@gmail.com>
2021-04-15 21:57:52 +0200

    styled navbar toggler and post buton

--- frontend/src/pages/posts/Form.jsx
+++ frontend/src/pages/posts/Form.jsx
@@ -25,7 +25,7 @@ export default function PostForm({ onSubmit }) {
           </div>
 
           <div className="form-group">
-            <button className="btn btn-primary" onClick={handleSubmit}>
+            <button className="btn btn-success" onClick={handleSubmit}>
               Post
             </button>
           </div>
