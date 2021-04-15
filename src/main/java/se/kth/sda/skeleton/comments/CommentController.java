23923e6242b5d47fe48e283e76acc2c05e62ad19
Yash Seeta <yashseeta94@gmail.com>
2021-04-15 13:07:16 +0200

    Pull and use resourcenotfoundexecption

--- src/main/java/se/kth/sda/skeleton/comments/CommentController.java
+++ src/main/java/se/kth/sda/skeleton/comments/CommentController.java
@@ -3,6 +3,7 @@ package se.kth.sda.skeleton.comments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import se.kth.sda.skeleton.ResourceNotFoundException;
 import se.kth.sda.skeleton.posts.PostRepository;
 
 import javax.validation.Valid;
@@ -24,14 +25,14 @@ public class CommentController{
     // Return the given comment
     @GetMapping("/comments/{id}")
     public ResponseEntity<Comment> getComment(@PathVariable Long id){
-        Comment commentToGet = commentRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        Comment commentToGet = commentRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
         return ResponseEntity.ok(commentToGet);
     }
 
     // Delete the given comment
     @DeleteMapping("/comments/{id}")
     public ResponseEntity<Comment> deleteComment(@PathVariable Long id){
-        Comment commentToBeDeleted = commentRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        Comment commentToBeDeleted = commentRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
         commentRepository.delete(commentToBeDeleted);
         return ResponseEntity.ok(commentToBeDeleted);
     }
@@ -39,7 +40,7 @@ public class CommentController{
     // Update a given comment
     @PutMapping("/comments/{id}")
     public ResponseEntity<Comment> updateComment(@PathVariable Long id, @Valid @RequestBody Comment updatedComment){
-        commentRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        commentRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
         updatedComment.setId(id);
         commentRepository.save(updatedComment);
         return ResponseEntity.ok(updatedComment);
