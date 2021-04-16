22cf0cc48dd92cdc355d2a6a3546db425aff1a12
Esther Chee <pnchee@gmail.com>
2021-04-16 15:35:47 +0200

    delete Title and AuthorName from Post

--- src/main/java/se/kth/sda/skeleton/posts/Post.java
+++ src/main/java/se/kth/sda/skeleton/posts/Post.java
@@ -13,50 +13,31 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
     private String body;
-    private String authorName;
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL)
     private List<Comment> comments;
+
     public Post() {
     }
 
-    public Post(String title, String body, String authorName) {
-        this.title = title;
+    public Post(String body) {
         this.body = body;
-        this.authorName = authorName;
     }
 
     public void setId(Long id) {
         this.id = id;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public void setBody(String body) {
         this.body = body;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getTitle() {
-        return title;
-    }
-
     public String getBody() {
         return body;
-    }
-
-    public String getAuthorName() {
-        return authorName;
     }
 
     public List<Comment> getComments() {
