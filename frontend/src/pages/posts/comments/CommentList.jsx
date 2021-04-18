97ac881e11097657256b5a41309088ca77325a51
En-Chi Liu <enchi.liu@gmail.com>
2021-04-18 12:07:59 +0200

    refactor out methods to be moved to Card.jsx

--- frontend/src/pages/posts/comments/CommentList.jsx
+++ frontend/src/pages/posts/comments/CommentList.jsx
@@ -1,36 +1,14 @@
 // NPM Packages
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 // Project files
 import Comment from "./Comment";
-import CommentsApi from "../../../api/CommentsApi";
 
-export default function CommentList({ post }) {
-    // Local state
-    const [comments, setComments] = useState([]);
-
+export default function CommentList({ comments, onDelete }) {
     // Components
-    const postId = post.id;
     const CommentsArray = comments.map((comment) => (
-        <Comment key={comment.id} comment={comment} onDeleteClick={() => deleteComment(comment)} />
+        <Comment key={comment.id} comment={comment} onDeleteClick={onDelete} />
     ));
-
-    // Methods
-    async function deleteComment(commentToDelete) {
-        try {
-            await CommentsApi.deleteComment(commentToDelete.id);
-            const newComments = comments.filter((comment) => comment.id !== commentToDelete.id);
-            setComments(newComments);
-        } catch(e) {
-            console.error(e);
-        }
-    }
-
-    useEffect( () => {
-        CommentsApi.getAllComments(postId)
-            .then(({data}) => setComments(data))
-            .catch((err) => console.error(err));
-    }, [setComments]);
 
     return (
         <div>
