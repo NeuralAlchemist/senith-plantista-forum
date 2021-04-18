3dedd2680bbac8f3b8fd01214a07ad9cd5723fa8
En-Chi Liu <enchi.liu@gmail.com>
2021-04-18 12:06:05 +0200

    add handleDelete helper to pass in comment as arg

--- frontend/src/pages/posts/comments/Comment.jsx
+++ frontend/src/pages/posts/comments/Comment.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 export default function Comment({ comment, onDeleteClick }) {
+    // Methods
+    const handleDelete = () => {
+        // Invoke the passed in event callback
+        onDeleteClick(comment);
+    };
     return (
         <div className="card mt-3">
             <div className="card-body">
                 <p>{comment.body}</p>
 
-                <button className="btn btn-danger" onClick={onDeleteClick}>
+                <button className="btn btn-danger" onClick={handleDelete}>
                     Delete
                 </button>
             </div>
