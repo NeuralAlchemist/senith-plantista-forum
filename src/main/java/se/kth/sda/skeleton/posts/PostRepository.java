f19b2d1cd77d1836e0b750dd7f68284b0634ada5
Yash Seeta <yashseeta94@gmail.com>
2021-04-19 13:42:51 +0200

    Add docs for post repository

--- src/main/java/se/kth/sda/skeleton/posts/PostRepository.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostRepository.java
@@ -2,7 +2,9 @@ package se.kth.sda.skeleton.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+/**
+ * Represents the database of domain type Post as a JPA Repository
+ */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 }
