aa4d9d6c14eafd48c4eb1d9468e754b35574eb3b
Yash Seeta <yashseeta94@gmail.com>
2021-04-19 16:21:35 +0200

    Edit comment to Comment where needed

--- src/main/java/se/kth/sda/skeleton/comments/Comment.java
+++ src/main/java/se/kth/sda/skeleton/comments/Comment.java
@@ -11,8 +11,7 @@ import javax.validation.constraints.NotNull;
 /**
  * Represents a comment on a {@link Post} as a JPA Entity. This implementation of comment will autogenerate a primary key of type
  * {@link Long} to indicate the current Comment entity. A Comment has a non-null field called {@code owner}. The {@code owner}
- * is a {@link Post} to which the Comment is related. The body of a {@code Comment} can be null. Comment shares a many to one
- * ORM relationship with Post class.
+ * is a {@link Post} to which the Comment is related. The body of a {@code Comment} can be null.
  */
 
 @Entity
