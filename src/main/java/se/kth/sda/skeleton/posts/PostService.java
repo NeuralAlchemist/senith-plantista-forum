c69748cb9a72754dbc50170e45f05ad0a7f48637
Yash Seeta <yashseeta94@gmail.com>
2021-04-19 14:14:49 +0200

    add docs for post service layer

--- src/main/java/se/kth/sda/skeleton/posts/PostService.java
+++ src/main/java/se/kth/sda/skeleton/posts/PostService.java
@@ -6,6 +6,9 @@ import se.kth.sda.skeleton.ResourceNotFoundException;
 
 import java.util.List;
 
+/**
+ * Represents the service layer. It contains defined functionalities according to business logic for Posts.
+ */
 @Service
 public class PostService {
 
@@ -16,19 +19,41 @@ public class PostService {
         this.postRepository = postRepository;
     }
 
+    /**
+     * Returns all Posts currently in the repository.
+     * @return a list of all posts currently in the repository.
+     */
     public List<Post> findAllPosts() {
         return postRepository.findAll();
     }
 
+    /**
+     * Returns a Post with the given {@code id} or throws a {@link ResourceNotFoundException} if there is no Post with the given
+     * {@code id}
+     * @param id the id used to find the Post associated with it
+     * @return Post associated with the given {@code id}
+     * @throws ResourceNotFoundException if there is no Post with the given {@code id}
+     */
     public Post findPostById(long id) {
         return postRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
+    /**
+     * Adds the given Post to the {@link PostRepository} and returns the added Post.
+     * @param post the Post to be added into the {@link PostRepository}
+     * @return Post added to the {@link PostRepository}
+     */
     public Post addPost(Post post) {
         return postRepository.save(post);
     }
 
+    /**
+     * Updates the Post with the given {@code id} with the given {@code newPost}.
+     * @param newPost the Post which will replace the Post at given {@code id}
+     * @param id the id used to find the Post to be updated
+     * @return
+     */
     public Post updatePost(Post newPost, long id) {
         Post post = findPostById(id);
         if (newPost.getBody() != null) {
@@ -37,6 +62,10 @@ public class PostService {
         return postRepository.save(post);
     }
 
+    /**
+     * Deletes Post with the given {@code id}
+     * @param id the id used to find the Post to be deleted
+     */
     public void deletePost(long id) {
         postRepository.deleteById(id);
     }
